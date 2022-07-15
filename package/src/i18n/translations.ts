06f7a34fdde4a22474c8d73eb901a8ace1968f04
JJ Uzumaki <104718343+xpunk7@users.noreply.github.com>
2022-07-15 10:45:15 +0200

    Create translations.ts

---
+++ package/src/i18n/translations.ts
@@ -0,0 +1,53 @@
+export const resources = {
+  en: {
+    translation: {
+      tabs: {
+        buy: "Buy",
+        sell: "Sell",
+      },
+      buyCryptoView: {
+        heading: "How much do you want to buy?",
+        fetchingPrice: "Fetching best price...",
+        amountYouPay: "Amount you pay: ",
+        youGet: "You get ",
+        paymentMethod: "Payment method",
+        bestRate: "Best rate via",
+        bestPerformance: "Best performance via",
+        gatewayByUserChoice: "Best rate via",
+        includeAllFees: "Includes all fees",
+        buy: "Buy",
+      },
+      footer: {
+        exohoodPrefix: "Powered by",
+      },
+      menu: {
+        title: "Menu",
+        menuItems: {
+          faq: "FAQ",
+          privacyPolicy: "Privacy Policy",
+          terms: "Terms of Usage",
+          help: "Help & Support",
+          support: {
+            moonpay: "MoonPay Support",
+            uniswap: "Uniswap Support",
+            btcdirect: "BTCDirect Support",
+            coinify: "Coinify Support",
+            indacoin: "Indacoin Support",
+            mercuryo: "Mercuryo Support",
+            utorg: "Utorg Support",
+            wyre: "Wyre Support",
+            xanpool: "Xanpool Support",
+          },
+        },
+      },
+      header: {
+        selectFiat: "Select fiat currency",
+        selectCrypto: "Select cryptocurrency",
+      },
+      button: {
+        continue: "Continue",
+        sending: "Sending...",
+      },
+    },
+  },
+};
